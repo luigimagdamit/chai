84255d7251bcbd32bbbc35a75670a1b4b24175dd
Luigi <luigiantonmagdamit@gmail.com>
2024-11-25 19:10:59 -0800

    number parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,65 @@
 mod token;    // Import the Token and TokenType module
 mod scanner;  // Import the Scanner module
-use std::fmt;
 use scanner::Scanner;
 use token::{TokenType, ErrorCode, Token};
 
+enum Precedence {
+    PrecNone,
+    PrecAssignment,
+    PrecAnd,
+    PrecEquality,
+    PrecComparison,
+    PrecTerm,
+    PrecFactor,
+    PrecUnary,
+    PrecCall,
+    PrecPrimary
+}
+impl Precedence {
+    // Convert a number to a Precedence enum variant
+    fn from_u32(value: u32) -> Precedence {
+        match value {
+            0 => (Precedence::PrecNone),
+            1 => (Precedence::PrecAssignment),
+            2 => (Precedence::PrecAnd),
+            3 => (Precedence::PrecEquality),
+            4 => (Precedence::PrecComparison),
+            5 => (Precedence::PrecTerm),
+            6 => (Precedence::PrecFactor),
+            7 => (Precedence::PrecUnary),
+            8 => (Precedence::PrecCall),
+            9 => (Precedence::PrecPrimary),
+            _ => Precedence::PrecNone
+
+        }
+    }
+    fn to_u32(&self) -> u32 {
+        match self {
+            Precedence::PrecNone => 0,
+            Precedence::PrecAssignment => 1,
+            Precedence::PrecAnd => 2,
+            Precedence::PrecEquality => 3,
+            Precedence::PrecComparison => 4,
+            Precedence::PrecTerm => 5,
+            Precedence::PrecFactor => 6,
+            Precedence::PrecUnary => 7,
+            Precedence::PrecCall => 8,
+            Precedence::PrecPrimary => 9,
+        }
+    }
+}
 struct Parser<'a>{
     current: Option<token::Token<'a>>,
     previous: Option<token::Token<'a>>,
-    scanner: Scanner<'a>
+    scanner: Scanner<'a>,
+    had_error: bool,
+    panic_mode: bool,
+    left_hand: Option<Leaf>,
+    right_hand: Option<Leaf>
 }
 impl<'a>Parser <'a>{
-    fn errorAt(&self, token: &Token, message: &str) {
+    fn error_at(&mut self, token: &Token, message: &str) {
+        self.panic_mode = true;
         let stderr = format!("Line: {} - ", token.line);
         match token.token_type {
             TokenType::EOF => {
@@ -20,35 +69,51 @@ impl<'a>Parser <'a>{
                 println!("{} {} at `{}...`", stderr, token.start, &self.scanner.get_lexeme(loc));
             },
             _ => {
-                println!("{} at `{}`", stderr, token.start);
+                println!("{} {}  at `{}`", stderr, message, token.start);
             }
+        }
+        if self.panic_mode {
+            return
         }
     } 
     fn advance(&mut self) {
         self.previous = self.current.take();
         
-        println!("Scanner State");
-        while let token = self.get_token() {
+        // println!("Scanner State");
+        loop {
+            let token = self.get_token();
             match token.token_type {
                 TokenType::Error(_)=> {
-                    self.errorAt(&token, token.start);
+                    self.error_at(&token, token.start);
                 },
                 _ => {
                     self.current = Some(token);
-                    match self.previous {
-                        Some(t) => println!("\t{}", t),
-                        _ => println!("\t<no token>")
-                    }
-                    match self.current {
-                        Some(t) => println!("\t{}", t),
-                        _ => println!("<\tno token>")
-                    }
+                    // match self.previous {
+                    //     Some(t) => println!("\t<{}>", t),
+                    //     _ => println!("\t<no token>")
+                    // }
+                    // match self.current {
+                    //     Some(t) => println!("\t<{}>", t),
+                    //     _ => println!("<\tno token>")
+                    // }
 
                     break;
                 }
             }
         }
 
+    }
+    fn consume(&mut self, token_type: TokenType, message: &str) {
+        if let Some(token) = self.current {
+            match token.token_type {
+                _ if token.token_type != token_type => {
+                    self.error_at(&token, message);
+                },
+                _ => {
+                    self.advance();
+                }
+            }
+        }
     }
     fn get_token(&mut self) -> Token<'a> {
         return self.scanner.scan_token().clone()
@@ -57,57 +122,153 @@ impl<'a>Parser <'a>{
         Parser {
             current: None,
             previous: None,
-            scanner: Scanner::init_scanner(&source)
+            scanner: Scanner::init_scanner(&source),
+            panic_mode: false,
+            had_error: false,
+            left_hand: None,
+            right_hand: None
         }
     }
     fn compile(&mut self) {
         self.advance();
+        expression(self);
+        self.consume(TokenType::EOF, "Expect end of expression");
+
     }
 }
-impl <'a> fmt::Display for Parser<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match (self.previous, self.current) {
-            (Some(t1), Some(t2)) => {
-                write!(f, "{} {}", t1, t2);
-            }
-            (Some(t1), None) => {
-                write!(f, "{} {}", t1, "<no token>");
+
+enum DataType {
+    Integer(String)
+}
+enum Operator {
+    Add
+}
+struct Leaf {
+    left: String,
+    right: String,
+    data_type: DataType
+}
+impl Leaf {
+    fn print_leaf(&self) {
+        println!("<leaf> <left: {}> <right: {}>", self.left, self.right);
+    }
+}
+struct Root {
+    left: Leaf,
+    right: Leaf,
+    operator: Operator
+}
+fn parse_number(parser: &mut Parser) {
+    let value = parser.previous.unwrap().start;
+    let number_leaf = Leaf {
+        left: String::from(format!("i32 {}", value)),
+        right: String::from(value),
+        data_type: DataType::Integer((String::from(value)))
+    };
+    &number_leaf.print_leaf();
+    match parser.left_hand {
+        None => parser.left_hand = Some(number_leaf),
+        Some(_) => parser.right_hand = Some(number_leaf)
+    }
+    
+    println!("<number: {}>", value)
+}
+fn expression(parser: &mut Parser) {
+    parse_precedence(parser, Precedence::PrecAssignment);
+}
+fn parse_precedence(parser: &mut Parser, precedence: Precedence) {
+    parser.advance();
+    // println!("AHHH {}", parser.previous.unwrap());
+    if let Some(prev) = parser.previous {
+        let prefix_rule = get_rule(prev.token_type).prefix;
+        match prefix_rule {
+            None => { 
+                 panic!()
             },
-            (None, Some(t2)) => {
-                write!(f, "{} {}", "<no token>", t2);
-            }
-            (None, None) => {
-                write!(f, "{} {}", "<no token>", "<no token>");
+            Some(prefix_fn) => {
+                prefix_fn(parser);
             }
         }
-        match self.current {
-            Some(token) => write!(f, "{}", token),
-            _ => write!(f, "<no token>")
+        if let Some(curr) = parser.current {
+            // println!("aaaaa{}", curr.token_type);
+            // println!("Status {} {}", parser.previous.unwrap(), parser.current.unwrap());
+            // println!("Prec {} {}", precedence.to_u32(), get_rule(curr.token_type).precedence.to_u32());
+            while precedence.to_u32() <= get_rule(curr.token_type).precedence.to_u32() {
+                
+                parser.advance();
+
+                if let Some(infix_rule) = get_rule(parser.previous.unwrap().token_type).infix {
+
+                    infix_rule(parser);
+                } else {
+                    break
+                }
+            }
         }
-        // write!(f, "Previous: {} Current: {}", self.previous.unwrap(), self.current)
     }
+    
+}
+fn parse_binary(parser: &mut Parser) {
+    if let Some(token) = parser.previous {
+       
+        let operator_type = token.token_type;
+        let rule_fn = get_rule(operator_type);
+        let new_prec = rule_fn.precedence as u32;
+        parse_precedence(parser, Precedence::from_u32(new_prec + 1));
+
+        if let Some(left) = &parser.left_hand {
+            left.print_leaf();
+        }
+        if let Some(right) = &parser.right_hand {
+            right.print_leaf();
+        }
+        match operator_type {
+            TokenType::Plus => println!("<plus>"),
+            _ => {}
+        }
+
+
+    }
+    
+}
+fn parse_grouping(parser: &mut Parser) {
+    expression(parser);
+    parser.consume(TokenType::RightParen, "Expect ')' after expression");
 }
 
-fn main() {
-    let source = "+=!===";
-    //let scanner = &mut Scanner::init_scanner();
 
-    // while !scanner.is_at_end() {
-    //     let s = scanner.scan_token();
-    //     println!("{} {}", s.token_type, s.start);
-    //     match s.token_type {
-    //         TokenType::Error(location) => {
-    //             println!("[ {} ] {} ", ErrorCode::SyntaxError, &scanner.get_lexeme(location))
-    //         }
-    //         TokenType::EOF => { break }
-    //         _ => {}
-    //     }
-    // }
-    // println!("Scanner has gracefully exited: {}", scanner.is_at_end());
+fn get_rule<'a>(token_type: TokenType) -> ParseRule<'a> {
+    // println!("GetRule: {}", token_type);
+    match token_type {
+        TokenType::Plus => {
+
+            let add_rule = ParseRule { prefix: None, infix: Some(parse_binary), precedence: Precedence::PrecTerm };
+            if let Some(r) = add_rule.infix {
+
+            }
+            return add_rule
+        },
+        TokenType::Number => ParseRule { prefix: Some(parse_number), infix: None, precedence: Precedence::PrecNone },
+        TokenType::EOF => ParseRule {prefix: None, infix: None, precedence: Precedence::PrecNone },
+        _ => ParseRule { prefix: None, infix: None, precedence: Precedence::PrecNone } 
+    }
+}
+struct ParseRule<'a>{
+    prefix: Option<ParseFn<'a>>,
+    infix: Option<ParseFn<'a>>,
+    precedence: Precedence,
+}
+type ParseFn<'a> = fn(&'a mut Parser);
+fn main() {
+    let source = "69+320+3000";
+
+
     let parser = &mut Parser::init_parser(source);
     
-    while !parser.scanner.is_at_end() {
-        parser.compile();
-    }
+    // while !parser.scanner.is_at_end() {
+    //     parse_number(parser);
+    // }
+    parser.compile(); // warmup
+
 
 }
