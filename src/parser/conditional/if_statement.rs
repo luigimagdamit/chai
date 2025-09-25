93081daeffa43a10062b8dda356dd9b4b7fbb551
Luigi Magdamit <luigiantonmagdamit@gmail.com>
2025-09-25 13:22:17 -0700

    if statement refactor

--- src/parser/conditional/if_statement.rs
+++ src/parser/conditional/if_statement.rs
@@ -7,6 +7,7 @@ use crate::parser::{
     parse_fn::declaration,
     parse_fn::statement
 };
+use crate::parser::expression::expr::Expr;
 use crate::scanner::token::TokenType;
 
 pub enum LlvmConditional {
@@ -66,34 +67,69 @@ impl LlvmConditional {
     }
     
 }
-pub fn if_statement(parser: &mut Parser) {
-    // if keyworld already consumed
-    // parse expression
-    parser.expr_count += 3;
-    let depth = parser.depth;
-    expression(parser);
-    let expr = parser.expr_pop();
-    let branch = LlvmConditional::If(depth);
-    parser.comment(&format!("depth: {}", depth).to_string());
-    parser.depth += 1;
-    parser.emit_instruction(&branch.create_branch(expr.1+1));
-    parser.consume(TokenType::LeftBrace, "message");
-    parser.emit_instruction(&branch.then_branch());
-    // parse block
-    while !parser.match_current(TokenType::RightBrace) {
-        declaration(parser);
+struct IfStatement {
+    expr: Option<Expr>,
+    branch_struct: Option<LlvmConditional>,
+    count: u32,
+    depth: u32,
+}
+impl IfStatement {
+    fn new() -> IfStatement {
+        IfStatement { expr: None, branch_struct: None, count: 0, depth: 0 }
     }
-    parser.emit_instruction(&&branch.to_end());
-    parser.emit_instruction(&branch.else_branch());
-    
-    if parser.match_current(TokenType::Else) {
-        parser.consume(TokenType::LeftBrace, "message");
-        while !parser.match_current(TokenType::RightBrace) {
-            declaration(parser);
+    fn init(&mut self, parser: &mut Parser) -> &mut IfStatement {
+        parser.expr_count += 3;
+        self.depth = parser.depth;
+        expression(parser);
+        let (expr, count) = parser.expr_pop();
+        (self.expr, self.count) = (Some(expr), count);
+        self.branch_struct = Some(LlvmConditional::If(self.depth));
+        parser.comment(&format!("depth: {}", self.depth).to_string());
+        parser.depth += 1;
+        self
+    }
+    fn then_branch(&mut self, parser: &mut Parser) -> &mut IfStatement {
+        if let Some(branch) = &self.branch_struct {
+            parser.emit_instruction(&branch.create_branch(self.count+1));
+            parser.consume(TokenType::LeftBrace, "message");
+            parser.emit_instruction(&branch.then_branch());
+            // parse block
+            while !parser.match_current(TokenType::RightBrace) {
+                declaration(parser);
+            }
+            parser.emit_instruction(&&branch.to_end());
         }
+        self
     }
-    parser.emit_instruction(&&branch.to_end());
-    parser.emit_instruction(&branch.end_branch());
-    parser.depth -= 1;
+    fn else_branch(&mut self, parser: &mut Parser) -> &mut IfStatement {
+        if let Some(branch) = &self.branch_struct {
+            // Emit the else branch instruction
+            parser.emit_instruction(&branch.else_branch());
 
+            // Handle an explicit `else { ... }` block
+            if parser.match_current(TokenType::Else) {
+                parser.consume(TokenType::LeftBrace, "Expected '{' after 'else'");
+                while !parser.match_current(TokenType::RightBrace) {
+                    declaration(parser);
+                }
+            }
+            parser.emit_instruction(&branch.to_end());
+            
+        }
+        self
+    }
+    fn end_branch(&mut self, parser: &mut Parser) -> &mut IfStatement {
+        if let Some(branch) = &self.branch_struct {
+            parser.emit_instruction(&branch.end_branch());
+            parser.depth -= 1;
+        }
+       self
+    }
+}
+pub fn if_statement(parser: &mut Parser) {
+    IfStatement::new()
+        .init(parser).
+        then_branch(parser)
+        .else_branch(parser)
+        .end_branch(parser);
 }
